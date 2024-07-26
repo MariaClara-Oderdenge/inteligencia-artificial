31cc472176214fd846829b6ea7baf4866862319f
Maria Clara ☆ <104856731+MariaClara-Oderdenge@users.noreply.github.com>
2024-07-26 13:30:07 +0000

    .

---
+++ main.js
@@ -0,0 +1,28 @@
+const perguntas = [ 
+    { 
+        enunciado: "Você gosta de bolo?",
+        alternativas: [
+            {
+                 texto:"Sim",
+                 afirmacao: "Você é uma pessoa nomal."
+            }, 
+            { 
+                texto:"Não",
+                afirmacao: "Você é chato pra carambolas."
+            }]
+    }, 
+
+    { 
+        enunciado: "Você dorme?",
+        alternativas: [
+            {
+                 texto:"Sim",
+                 afirmacao: "Você é uma pessoa nomal."
+            }, 
+            { 
+                texto:"Não",
+                afirmacao: "Você é uma anomalia do universo."
+            }]
+
+    },
+]
